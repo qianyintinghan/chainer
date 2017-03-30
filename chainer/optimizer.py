e14a6f455c6c6d76bddc8405e2b39097476014a4
Seiya Tokui <beam.web@gmail.com>
2017-03-30 18:02:33 +0900

    Fix Hyperparameter docstring

--- chainer/optimizer.py
+++ chainer/optimizer.py
@@ -32,10 +32,11 @@ def exponential_decay_noise(xp, shape, dtype, hook, opt):
 
 class Hyperparameter(object):
 
-    """Set of hyperparameters of an optimizer.
+    """Set of hyperparameter entries of an optimizer.
 
-    This is a utility class to provide a set of hyperparameters for update
-    rules and an optimizer. It can be used as a usual object.
+    This is a utility class to provide a set of hyperparameter entries for
+    update rules and an optimizer. Each entry can be set as an attribute of a
+    hyperparameter object.
 
     A hyperparameter object can hold a reference to its parent hyperparameter
     object. When an attribute does not exist in the child hyperparameter, it
